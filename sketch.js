d9e010f0cc2a8728d2132f20ca59fb44a7e5173f
Aarjav Chauhan <aarjavchauhan439@gmail.com>
2019-11-30 14:50:43 -0700

    added capital and inflow json. show capitals with circles

--- sketch.js
+++ sketch.js
@@ -15,11 +15,55 @@ const mappa = new Mappa('Mapbox', key);
 let myMap;
 let canvas;
 
+//array storing pixel locations of viewable capitals
+let capitalArray = [];
+
+
+function preload() {
+  data = loadJSON('country_capitals.json')
+}
 
 function setup() {
   canvas = createCanvas(700, 700);
+
   // Create a tile map and overlay the canvas on top.
   myMap = mappa.tileMap(options);
   myMap.overlay(canvas);
+  myMap.onChange(mapCapitals);
+}
 
+function draw() {
+  clear()
+  drawCircles()
+}
+
+function drawCircles(){
+  push()
+  noStroke()
+  fill(125, 125, 125)
+  for (var i = 0; i < capitalArray.length; i++) {
+    var latitude = capitalArray[i][0];
+    var longitude = capitalArray[i][1];
+    var radius = 10 + 10 * sin(frameCount / 50);
+    circle(latitude, longitude, radius)
+  }
+  pop()
+}
+
+//read JSON file
+//get the latitudes and longitudes of the capitals
+//convert and map them to pixels on the canvas
+//store them in the capitalArray
+function mapCapitals(){
+
+  capitalArray.length = 0;
+
+  for (var i = 0; i < 240; i++) {
+    const latitude = Number(data[i].CapitalLatitude);
+    const longitude = Number(data[i].CapitalLongitude);
+    if (myMap.map.getBounds().contains([latitude, longitude])) {
+      const position = myMap.latLngToPixel(latitude, longitude);
+      capitalArray.push([position.x, position.y]);
+    }
+  }
 }
