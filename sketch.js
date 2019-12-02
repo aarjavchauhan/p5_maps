4283ca22b8eec519a92aa3b3ddd2629089fed857
Aarjav Chauhan <aarjavchauhan439@gmail.com>
2019-12-02 11:36:17 -0700

    display text and legend with gradient

--- sketch.js
+++ sketch.js
@@ -43,9 +43,15 @@ function drawCircles(){
   let yearMap = Math.ceil(map(mouseX, 0, width, 0, 40))
   push()
   fill(0, 0, 0)
+  textAlign(CENTER)
   textSize(32)
   text(1980+yearMap, mouseX, mouseY-10)
   pop()
+
+  drawText()
+
+  displayLegend()
+
   push()
   noStroke()
   for (var i = 0; i < capitalArray.length; i++) {
@@ -71,7 +77,6 @@ function mapCapitals(){
     const latitude = Number(data[i].CapitalLatitude);
     const longitude = Number(data[i].CapitalLongitude);
 
-    var d1980 = 0
     var yearArray = []
 
     for (var j = 1980; j <= 2019; j++) {
@@ -86,6 +91,44 @@ function mapCapitals(){
     if (myMap.map.getBounds().contains([latitude, longitude])) {
       const position = myMap.latLngToPixel(latitude, longitude);
       capitalArray.push([position.x, position.y, yearArray]);
+    }
+  }
+}
+
+function drawText(){
+  push()
+    fill(0, 0, 0)
+    textSize(32)
+    textAlign(CENTER)
+    text('Remittance Inflow', width/2, height/10)
+  pop()
+}
+
+function displayLegend(){
+  push()
+    noStroke()
+    var startColor = color(0, 142, 204, 125)
+    var endColor = color(194,24,7, 125)
+    setGradient(width/5, height-height/10, 3*width/5, height/40, startColor, endColor, "X")
+  pop()
+}
+
+//p5js example function to create gradients with colors over a length or height
+function setGradient(x, y, w, h, c1, c2, axis) {
+  noFill();  if (axis == "Y") {  // Top to bottom gradient
+    for (let i = y; i <= y+h; i++) {
+      var inter = map(i, y, y+h, 0, 1);
+      var c = lerpColor(c1, c2, inter);
+      stroke(c);
+      line(x, i, x+w, i);
+    }
+  }
+  else if (axis == "X") {  // Left to right gradient
+    for (let j = x; j <= x+w; j++) {
+      var inter2 = map(j, x, x+w, 0, 1);
+      var d = lerpColor(c1, c2, inter2);
+      stroke(d);
+      line(j, y, j, y+h);
     }
   }
 }
